6ec16dcfc9cb68fadcb407b7b4dfc5b480ec6ee9
Joan Font <joanfont@gmail.com>
2015-12-01 22:19:11 +0100

    First commit

---
+++ antopo/core.py
@@ -0,0 +1,50 @@
+from xml.etree import ElementTree
+from xml.etree.ElementTree import tostring
+import functools
+from antopo.entities import Message
+
+
+class BaseStrings:
+    DEFAULT_TARGET_FILE_PREFIX = 'messages_'
+
+    def __init__(self, path, source_language, target_language, target_prefix=None):
+        self.path = path
+        self.source_language = source_language
+        self.target_language = target_language
+
+        self.target_prefix = target_prefix if target_prefix else self.DEFAULT_TARGET_FILE_PREFIX
+
+        self.messages = self.parse()
+
+    def parse(self):
+        raise NotImplemented
+
+
+class StringsXML(BaseStrings):
+    def parse(self):
+        tree = ElementTree.parse(self.path)
+        strings = tree.findall('./string')
+        build_message_fnx = functools.partial(Message.from_node, self.source_language)
+        return map(build_message_fnx, strings)
+
+    def translate(self):
+        def _translate_item(item):
+            return tostring(item.to_translated_xml())
+
+        return map(_translate_item, self.messages)
+
+    def get_target_filename(self):
+        return '{target_prefix}{target_language}.po'.format(target_pefix=self.target_prefix, target=languageself.target_language)
+
+    def build_translation_po(self):
+        target_filename = self.get_target_filename()
+        with open(target_filename, 'w+') as f:
+            for message in self.messages:
+                po_message = message.to_po()
+                po_block = '{}\n\n'.format(po_message)
+                f.write(po_block)
+
+
+class StringsPO(BaseStrings):
+    def parse(self):
+        pass
---
+++ antopo/entities.py
@@ -0,0 +1,44 @@
+from xml.etree.ElementTree import Element
+
+
+class BaseEntity:
+    pass
+
+
+class Message(BaseEntity):
+    def __init__(self, language, context=None, original=None, translation=None):
+        self.language = language
+        self.context = context
+        self.original = original
+        self.translation = translation
+
+    @classmethod
+    def from_node(cls, language, node):
+        context = node.get('name')
+        original = node.text
+        return cls(language, context=context, original=original)
+
+    def to_original_xml(self):
+        return self._to_xml(self.original)
+
+    def to_translated_xml(self):
+        return self._to_xml(self.translation)
+
+    def _to_xml(self, text):
+        xml_element = Element('string')
+
+        if self.context:
+            xml_element.set('name', self.context)
+
+        xml_element.text = text
+
+        return xml_element
+
+    def to_po(self):
+        translation = self.translation if self.translation is not None else ''
+        return '''msgctxt "{context}"\nmsgid "{original}"\nmsgstr "{translation}"'''.format(context=self.context,
+                                                                                            original=self.original,
+                                                                                            translation=translation)
+
+    def __repr__(self):
+        return '{}: {}'.format(self.context, self.original)
